a7c7eaf0b560f682cfc1d647908e06fd2f712abe
Shaobo ZHANG <hyperzsb@qq.com>
2022-12-28 12:18:55 +0800

    feat(go): complete tasks of go/gophercises quiz game part 2
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

--- go/gophercises/quiz-game/main.go
+++ go/gophercises/quiz-game/main.go
@@ -1,26 +1,16 @@
 package main
 
 import (
-	"bufio"
 	"encoding/csv"
 	"flag"
 	"fmt"
 	"gophercises/quizgame/question"
 	"log"
 	"os"
-	"strings"
+	"time"
 )
 
-func QuizGame() error {
-	// Define cli flags
-	var (
-		filename string
-	)
-
-	// Parse cli flags
-	flag.StringVar(&filename, "file", ".data/questions.csv", "question file in .csv format")
-	flag.Parse()
-
+func Quiz(filename string, questionNum, correctNum *int) error {
 	// Open file
 	file, err := os.Open(filename)
 	if err != nil {
@@ -28,43 +18,95 @@ func QuizGame() error {
 	}
 	defer file.Close()
 
-	// Parse and print questions per line
-	q, questionNum, correctNum := question.Question{}, 0, 0
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		csvReader := csv.NewReader(strings.NewReader(scanner.Text()))
-		csvRecord, err := csvReader.Read()
-		if err != nil {
+	csvReader := csv.NewReader(file)
+	csvRecords, err := csvReader.ReadAll()
+	if err != nil {
+		return err
+	}
+
+	questions := make([]question.Question, 0, len(csvRecords))
+	for idx, record := range csvRecords {
+		questions = append(questions, question.Question{})
+		if err := questions[idx].Parse(record); err != nil {
 			return err
 		}
+	}
+	*questionNum = len(questions)
 
-		if err := q.Parse(csvRecord); err != nil {
-			return err
-		}
-		questionNum++
-
-		fmt.Printf("Q #%d. %d%s%d = ", questionNum, q.Operand[0], string(q.Operator), q.Operand[1])
-
+	for i, q := range questions {
+		// Get user input and validate it
 		userAns := 0
+		fmt.Printf("Q #%d. %d%s%d = ", i+1, q.Operand[0], string(q.Operator), q.Operand[1])
 		_, err = fmt.Scanf("%d", &userAns)
 		for err != nil {
 			fmt.Printf("Please provide a valid numeric answer: %s\n", err)
-			fmt.Printf("%s=", csvRecord[0])
+			fmt.Printf("Q #%d. %d%s%d = ", i+1, q.Operand[0], string(q.Operator), q.Operand[1])
 			_, err = fmt.Scanf("%d", &userAns)
 		}
 
 		if userAns == q.Result {
-			correctNum++
+			*correctNum++
 		}
 	}
-
-	fmt.Printf("Your score: %d/%d\n", correctNum, questionNum)
 
 	return nil
 }
 
+func QuizGame() error {
+	// Define cli flags
+	var (
+		filename string
+	)
+	// Parse cli flags
+	flag.StringVar(&filename, "file", ".data/questions.csv", "question file in .csv format")
+	flag.Parse()
+
+	questionNum, correctNum := 0, 0
+	if err := Quiz(filename, &questionNum, &correctNum); err != nil {
+		return err
+	} else {
+		fmt.Printf("\nQuiz completed! Your score: %d out of %d\n", correctNum, questionNum)
+		return nil
+	}
+}
+
+func QuizGameWithTimer() error {
+	// Define cli flags
+	var (
+		filename  string
+		timeLimit time.Duration
+	)
+	// Parse cli flags
+	flag.StringVar(&filename, "file", ".data/questions.csv", "question file in .csv format")
+	flag.DurationVar(&timeLimit, "time", 30*time.Second, "time limit for the quiz")
+	flag.Parse()
+
+	done := make(chan bool)
+	errChan := make(chan error)
+
+	questionNum, correctNum := 0, 0
+	go func() {
+		if err := Quiz(filename, &questionNum, &correctNum); err != nil {
+			errChan <- err
+		} else {
+			done <- true
+		}
+	}()
+
+	select {
+	case <-done:
+		fmt.Printf("\nQuiz completed! Your score: %d out of %d\n", correctNum, questionNum)
+		return nil
+	case err := <-errChan:
+		return err
+	case <-time.After(timeLimit):
+		fmt.Printf("\nTime up! Your score: %d out of %d\n", correctNum, questionNum)
+		return nil
+	}
+}
+
 func main() {
-	if err := QuizGame(); err != nil {
+	if err := QuizGameWithTimer(); err != nil {
 		log.Fatal(err)
 	}
 }
