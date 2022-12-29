6c23333ecf64f4a1b62297f98b6d20bab49134ed
Shaobo ZHANG <hyperzsb@qq.com>
2022-12-29 17:57:50 +0800

    feat(go): implement map_handler of go/gophercises url shortener
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

--- go/gophercises/url-shortener/handler/map_handler.go
+++ go/gophercises/url-shortener/handler/map_handler.go
@@ -1,9 +1,20 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) (http.HandlerFunc, error) {
-	return nil, nil
+	if fallback == nil {
+		return nil, fmt.Errorf("fallback handler not specified")
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		if target, exist := pathsToUrls[r.URL.Path]; exist {
+			http.Redirect(w, r, target, 303)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}, nil
 }
