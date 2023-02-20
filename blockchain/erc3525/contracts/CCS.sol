b24c0910b85dbc54a8cc37730a1e4d5b7b5ac858
Shaobo ZHANG <hyperzsb@qq.com>
2023-02-20 13:59:42 +0800

    feat(blockchain/erc3525): add basic token-related features
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

--- blockchain/erc3525/contracts/CCS.sol
+++ blockchain/erc3525/contracts/CCS.sol
@@ -14,6 +14,7 @@ contract CCS is ERC3525 {
     /**
      * @notice This part is for slot-related features, including
      *  - Defination
+     *  - Allocation
      */
 
     mapping(uint256 => string) private slots;
@@ -38,7 +39,7 @@ contract CCS is ERC3525 {
      * @return The name associated with the given slot number, or "UNDEFINED" if the slot has not been defined
      * @dev If the slot has not been defined, the function returns "UNDEFINED".
      */
-    function slotInfo(uint _slot) public view returns (string memory) {
+    function slotInfo(uint256 _slot) public view returns (string memory) {
         if (bytes(slots[_slot]).length == 0) {
             return "UNDEFINED";
         } else {
@@ -46,12 +47,44 @@ contract CCS is ERC3525 {
         }
     }
 
-    function slotAllocate(uint _slot, address _account) external {
+    /**
+     * @notice Allocates the specified slot to the specified authority.
+     * @param _slot The slot to be allocated
+     * @param _account The authority to allocate the slot to
+     * @dev Only the owner can allocate slots to authorities.
+     * @dev The authority must be registered.
+     * @dev The slot must be defined before it can be allocated.
+     */
+    function slotAllocate(uint256 _slot, address _account) external {
         require(msg.sender == owner, "only the owner can allocate slots");
         require(isAuthority(_account), "authority is never registered");
         require(bytes(slots[_slot]).length > 0, "slot is never defined");
 
         authorities[_account].slots.push(_slot);
+    }
+
+    /**
+     * @notice Checks whether the specified slot is allocated to the specified authority.
+     * @param _slot The slot to check
+     * @param _account The authority to check
+     * @return A boolean indicating whether the slot is allocated to the authority
+     * @dev The slot must be defined before it can be checked.
+     * @dev The authority must be registered.
+     */
+    function isSlotAllocatedTo(
+        uint256 _slot,
+        address _account
+    ) public view returns (bool) {
+        require(bytes(slots[_slot]).length > 0, "slot is never defined");
+        require(isAuthority(_account), "authority is never registered");
+
+        for (uint256 i = 0; i < authorities[_account].slots.length; i++) {
+            if (authorities[_account].slots[i] == _slot) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
@@ -72,7 +105,7 @@ contract CCS is ERC3525 {
 
     mapping(address => Authority) private authorities;
 
-    uint public experiationTime = 365 * 24 * 60 * 60;
+    uint256 public experiationTime = 365 * 24 * 60 * 60;
 
     /**
      * @notice Registers a new authority with the given account, name, and domain
@@ -144,7 +177,7 @@ contract CCS is ERC3525 {
     function authorityInfo(
         address _account
     ) public view returns (Authority memory) {
-        require(isAuthority(_account), "it is no authority");
+        require(isAuthority(_account), "authority is never registered");
 
         return authorities[_account];
     }
@@ -165,5 +198,238 @@ contract CCS is ERC3525 {
         /// @custom:todo Add verification procedures, using oracles to validate the ownership of the domain
 
         authorities[_account].lastCheck = block.timestamp;
+    }
+
+    /**
+     * @notice This part is for token-related features, including
+     *  - Mintage
+     *  - Distribution
+     */
+
+    /**
+     * @notice Mints a new token with the specified value and assigns it to the calling authority in the specified slot.
+     * @param _slot The slot in which the token will be created
+     * @param _value The value of the token to be created
+     * @return The ID of the newly minted token
+     * @dev Only an authority can mint a new token and assign it to a slot.
+     * @dev The authority must be registered and valid.
+     * @dev The slot must be allocated to the calling authority.
+     */
+    function mint(uint256 _slot, uint256 _value) external returns (uint256) {
+        require(isAuthority(msg.sender), "authority is never registered");
+        require(isAuthorityValid(msg.sender), "authority is not vaild");
+        require(
+            isSlotAllocatedTo(_slot, msg.sender),
+            "slot is not alloctaed to authority"
+        );
+
+        return _mint(msg.sender, _slot, _value);
+    }
+
+    /**
+     * @notice These following functions should be overrided to deny non-suthority callers
+     */
+
+    /**
+     * @notice Approves another address to transfer the specified token on behalf of the token owner.
+     * @param _tokenId The ID of the token to be approved for transfer
+     * @param _to The address that will be approved to transfer the token
+     * @param _value The amount of tokens to be approved for transfer
+     * @dev Only an authority can approve a transfer of tokens on behalf of the owner.
+     * @dev The authority must be registered and valid.
+     */
+    function approve(
+        uint256 _tokenId,
+        address _to,
+        uint256 _value
+    ) public payable virtual override {
+        require(isAuthority(msg.sender), "authority is never registered");
+        require(isAuthorityValid(msg.sender), "authority is not vaild");
+
+        super.approve(_tokenId, _to, _value);
+    }
+
+    /**
+     * @notice Approves another address to transfer the specified token on behalf of the token owner.
+     * @param _to The address that will be approved to transfer the token
+     * @param _tokenId The ID of the token to be approved for transfer
+     * @dev Only an authority can approve a transfer of tokens on behalf of the owner.
+     * @dev The authority must be registered and valid.
+     */
+    function approve(
+        address _to,
+        uint256 _tokenId
+    ) public payable virtual override {
+        require(isAuthority(msg.sender), "authority is never registered");
+        require(isAuthorityValid(msg.sender), "authority is not vaild");
+
+        super.approve(_to, _tokenId);
+    }
+
+    /**
+     * @notice Gets the approved address for a token ID
+     * @param _tokenId The ID of the token to get the approved address for
+     * @return The approved address for the given token ID
+     * @dev Only an authority can get the approved address for a token.
+     * @dev The authority must be registered and valid.
+     */
+    function getApproved(
+        uint256 _tokenId
+    ) public view virtual override returns (address) {
+        require(isAuthority(msg.sender), "authority is never registered");
+        require(isAuthorityValid(msg.sender), "authority is not vaild");
+
+        return super.getApproved(_tokenId);
+    }
+
+    /**
+     * @notice Sets or unsets the approval of a given operator.
+     * @param _operator The operator whose approval is to be set or unset
+     * @param _approved True if the operator is to be approved, false if the approval is to be revoked
+     * @dev Only an authority can set or unset the approval of an operator.
+     * @dev The authority must be registered and valid.
+     */
+    function setApprovalForAll(
+        address _operator,
+        bool _approved
+    ) public virtual override {
+        require(isAuthority(msg.sender), "authority is never registered");
+        require(isAuthorityValid(msg.sender), "authority is not vaild");
+
+        super.setApprovalForAll(_operator, _approved);
+    }
+
+    /**
+     * @notice Returns whether the given operator is approved by a given owner.
+     * @param _owner The owner of the tokens
+     * @param _operator The operator to check for approval
+     * @return True if the operator is approved by the owner, false otherwise
+     * @dev Only an authority can check whether an operator is approved by an owner.
+     * @dev The authority must be registered and valid.
+     */
+    function isApprovedForAll(
+        address _owner,
+        address _operator
+    ) public view virtual override returns (bool) {
+        require(isAuthority(msg.sender), "authority is never registered");
+        require(isAuthorityValid(msg.sender), "authority is not vaild");
+
+        return super.isApprovedForAll(_owner, _operator);
+    }
+
+    /**
+     * @notice Returns the amount of tokens owned by a given address that are approved for transfer by another address.
+     * @param _tokenId The ID of the token to check the allowance for
+     * @param _operator The address of the operator to check the allowance for
+     * @return The amount of tokens approved for transfer by the operator for the given token ID
+     * @dev Only an authority can check the allowance for a token.
+     * @dev The authority must be registered and valid.
+     */
+    function allowance(
+        uint256 _tokenId,
+        address _operator
+    ) public view virtual override returns (uint256) {
+        require(isAuthority(msg.sender), "authority is never registered");
+        require(isAuthorityValid(msg.sender), "authority is not vaild");
+
+        return super.allowance(_tokenId, _operator);
+    }
+
+    /**
+     * @notice Transfers some value of a given token to another address.
+     * @param _fromTokenId The ID of the token to be transferred
+     * @param _to The address to transfer the token to
+     * @param _value The amount of value to transfer
+     * @return The new token ID of the transferred token
+     * @dev Only an authority can transfer a token.
+     * @dev The authority must be registered and valid.
+     */
+    function transferFrom(
+        uint256 _fromTokenId,
+        address _to,
+        uint256 _value
+    ) public payable virtual override returns (uint256) {
+        require(isAuthority(msg.sender), "authority is never registered");
+        require(isAuthorityValid(msg.sender), "authority is not vaild");
+
+        return super.transferFrom(_fromTokenId, _to, _value);
+    }
+
+    /**
+     * @notice Transfers some value of a given token to another token.
+     * @param _fromTokenId The ID of the token to be transferred
+     * @param _toTokenId The ID of the token to transfer to
+     * @param _value The amount of value to transfer
+     * @dev Only an authority can transfer a token.
+     * @dev The authority must be registered and valid.
+     */
+    function transferFrom(
+        uint256 _fromTokenId,
+        uint256 _toTokenId,
+        uint256 _value
+    ) public payable virtual override {
+        require(isAuthority(msg.sender), "authority is never registered");
+        require(isAuthorityValid(msg.sender), "authority is not vaild");
+
+        super.transferFrom(_fromTokenId, _toTokenId, _value);
+    }
+
+    /**
+     * @notice Transfers the ownership of a given token to another address.
+     * @param _from The address of the owner of the token
+     * @param _to The address to transfer the token to
+     * @param _tokenId The ID of the token to be transferred
+     * @dev Only an authority can transfer a token.
+     * @dev The authority must be registered and valid.
+     */
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) public payable virtual override {
+        require(isAuthority(msg.sender), "authority is never registered");
+        require(isAuthorityValid(msg.sender), "authority is not vaild");
+
+        super.transferFrom(_from, _to, _tokenId);
+    }
+
+    /**
+     * @notice Safely transfers the ownership of a given token ID to another address.
+     * @param _from The address of the owner of the token
+     * @param _to The address to transfer the token to
+     * @param _tokenId The ID of the token to be transferred
+     * @param _data Additional data with no specified format to be passed to the receiver contract
+     * @dev Only an authority can safely transfer a token.
+     * @dev The authority must be registered and valid.
+     */
+    function safeTransferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId,
+        bytes memory _data
+    ) public payable virtual override {
+        require(isAuthority(msg.sender), "authority is never registered");
+        require(isAuthorityValid(msg.sender), "authority is not vaild");
+
+        super.safeTransferFrom(_from, _to, _tokenId, _data);
+    }
+
+    /**
+     * @notice Safely transfers the ownership of a given token ID to another address.
+     * @param _from The address of the owner of the token
+     * @param _to The address to transfer the token to
+     * @param _tokenId The ID of the token to be transferred
+     * @dev Only an authority can safely transfer a token.
+     * @dev The authority must be registered and valid.
+     */
+    function safeTransferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) public payable virtual override {
+        require(isAuthority(msg.sender), "authority is never registered");
+        require(isAuthorityValid(msg.sender), "authority is not vaild");
+
+        super.safeTransferFrom(_from, _to, _tokenId);
     }
 }
---
+++ blockchain/erc3525/test/4_token.js
@@ -0,0 +1,228 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { expect } = require("chai");
+const { BigNumber } = require("ethers");
+
+describe("Token", function () {
+  async function CCSFixture() {
+    // Set the expiration time of the authority
+    const experiationTime = (await time.latest()) + 365 * 24 * 60 * 60;
+
+    // Get the contract's signers
+    const [owner, authority, user, others] = await ethers.getSigners();
+
+    // Deploy a new instance of the `CCS` contract
+    const CCS = await ethers.getContractFactory("CCS");
+    const ccs = await CCS.deploy();
+
+    // Return the contract instance and other variables as an object
+    return { ccs, owner, authority, user, others, experiationTime };
+  }
+
+  describe("Mintage", function () {
+    it("Should successfully mint a token", async function () {
+      const { ccs, authority } = await loadFixture(CCSFixture);
+
+      // Define a new slot
+      await ccs.slotDefine(3525, "ERC3525");
+
+      // Register the authority with the contract
+      const name = "Authority";
+      const domain = "authority.com";
+      await ccs.authorityRegister(authority.address, name, domain);
+
+      // Allocate the slot to the authority
+      await ccs.slotAllocate(3525, authority.address);
+
+      // Mint a new token and get its ID
+      const slot = 3525;
+      const value = 10;
+      const txResponse = await ccs.connect(authority).mint(slot, value);
+      const txRecipt = await txResponse.wait();
+      const tokenId = txRecipt.events.find(
+        (event) => event.event === "TransferValue"
+      ).args._toTokenId;
+
+      // Assert that the balance of the authority has increased
+      expect(await ccs["balanceOf(address)"](authority.address)).to.equal(1);
+
+      // Assert that the balance of the token is set to the value that was minted
+      expect(await ccs["balanceOf(uint256)"](tokenId)).to.equal(value);
+
+      // Assert that the token owner is the authority
+      expect(await ccs.ownerOf(tokenId)).to.equal(authority.address);
+
+      // Assert that the slot associated with the token is the one that was minted
+      expect(await ccs.slotOf(tokenId)).to.equal(slot);
+    });
+
+    it("Should be reverted if called by non-authority", async function () {
+      const { ccs, others } = await loadFixture(CCSFixture);
+
+      // Define a new slot
+      const slot = 3525;
+      await ccs.slotDefine(slot, "ERC3525");
+
+      const value = 10;
+      await expect(ccs.connect(others).mint(slot, value)).to.be.revertedWith(
+        "authority is never registered"
+      );
+    });
+
+    it("Should revert when authority is not valid", async function () {
+      // Load the fixture
+      const { ccs, authority, experiationTime } = await loadFixture(CCSFixture);
+
+      // Define a new slot
+      const slot = 3525;
+      await ccs.slotDefine(slot, "ERC3525");
+
+      // Register a new authority
+      const name = "Authority";
+      const domain = "authority.com";
+      await ccs.authorityRegister(authority.address, name, domain);
+
+      // Allocate the new slot to the authority
+      await ccs.slotAllocate(3525, authority.address);
+
+      // Increase time past the authority's expiration time
+      await time.increaseTo(experiationTime + 60);
+
+      // Verify that a non-valid authority cannot mint a new token
+      const value = 10;
+      await expect(ccs.connect(authority).mint(slot, value)).to.be.revertedWith(
+        "authority is not vaild"
+      );
+    });
+
+    it("Should be reverted if the slot is not allocated to the authority", async function () {
+      const { ccs, authority } = await loadFixture(CCSFixture);
+
+      // Define a new slot
+      const slot = 3525;
+      await ccs.slotDefine(slot, "ERC3525");
+
+      // Register the authority
+      const name = "Authority";
+      const domain = "authority.com";
+      await ccs.authorityRegister(authority.address, name, domain);
+
+      // Mint the token without slot allocation
+      const value = 10;
+      await expect(ccs.connect(authority).mint(slot, value)).to.be.revertedWith(
+        "slot is not alloctaed to authority"
+      );
+    });
+  });
+
+  describe("Distribution", function () {
+    it("Should successfully transfer token value to a user", async function () {
+      const { ccs, authority, user } = await loadFixture(CCSFixture);
+
+      await ccs.slotDefine(3525, "ERC3525");
+
+      const name = "Authority";
+      const domain = "authority.com";
+
+      await ccs.authorityRegister(authority.address, name, domain);
+
+      await ccs.slotAllocate(3525, authority.address);
+
+      const slot = 3525;
+      const originalValue = 10;
+
+      // Get the token id by emitted event
+      const mintTxResponse = await ccs
+        .connect(authority)
+        .mint(slot, originalValue);
+      const mintTxRecipt = await mintTxResponse.wait();
+      const mintTxEvents = mintTxRecipt.events.filter(
+        (event) => event.event === "TransferValue"
+      );
+      const tokenId = mintTxEvents[0].args._toTokenId;
+
+      const transferedValue = 1;
+
+      // Get the token ids by emitted event
+      const transferTxResponse = await ccs
+        .connect(authority)
+        ["transferFrom(uint256,address,uint256)"](
+          tokenId,
+          user.address,
+          transferedValue
+        );
+      const transferTxRecipt = await transferTxResponse.wait();
+      const transferTxEvents = transferTxRecipt.events.filter(
+        (event) => event.event === "TransferValue"
+      );
+
+      const fromTokenId = transferTxEvents[1].args._fromTokenId;
+      const toTokenId = transferTxEvents[1].args._toTokenId;
+
+      expect(tokenId).to.equal(fromTokenId);
+
+      expect(await ccs["balanceOf(uint256)"](fromTokenId)).to.equal(
+        originalValue - transferedValue
+      );
+
+      expect(await ccs["balanceOf(uint256)"](toTokenId)).to.equal(
+        transferedValue
+      );
+    });
+
+    it("Should be reverted if called by non-authority", async function () {
+      const { ccs, authority, user, others } = await loadFixture(CCSFixture);
+
+      await ccs.slotDefine(3525, "ERC3525");
+
+      const name = "Authority";
+      const domain = "authority.com";
+
+      await ccs.authorityRegister(authority.address, name, domain);
+
+      await ccs.slotAllocate(3525, authority.address);
+
+      const slot = 3525;
+      const originalValue = 10;
+
+      // Get the token id by emitted event
+      const mintTxResponse = await ccs
+        .connect(authority)
+        .mint(slot, originalValue);
+      const mintTxRecipt = await mintTxResponse.wait();
+      const mintTxEvents = mintTxRecipt.events.filter(
+        (event) => event.event === "TransferValue"
+      );
+      const tokenId = mintTxEvents[0].args._toTokenId;
+
+      const transferedValue = 1;
+
+      // Get the token ids by emitted event
+      const transferTxResponse = await ccs
+        .connect(authority)
+        ["transferFrom(uint256,address,uint256)"](
+          tokenId,
+          user.address,
+          transferedValue
+        );
+      const transferTxRecipt = await transferTxResponse.wait();
+      const transferTxEvents = transferTxRecipt.events.filter(
+        (event) => event.event === "TransferValue"
+      );
+
+      const toTokenId = transferTxEvents[1].args._toTokenId;
+
+      await expect(
+        ccs
+          .connect(user)
+          ["transferFrom(uint256,address,uint256)"](
+            toTokenId,
+            others.address,
+            transferedValue
+          )
+      ).to.be.revertedWith("authority is never registered");
+    });
+  });
+});
