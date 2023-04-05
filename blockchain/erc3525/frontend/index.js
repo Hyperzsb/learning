9c620d5d895ea3685bef47ba819a381770d4c4a9
Shaobo ZHANG <hyperzsb@qq.com>
2023-04-05 13:56:21 +0800

    feat(blockchain/erc3525): add slot-related features to the frontend app
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

---
+++ blockchain/erc3525/frontend/index.js
@@ -0,0 +1,157 @@
+import { ethers } from "https://cdnjs.cloudflare.com/ajax/libs/ethers/6.2.3/ethers.min.js";
+
+let provider = null;
+let signer = null;
+// Check if the MetaMask is installed
+if (window.ethereum == null) {
+  document.getElementById("wallet-status").innerText = "Not installed";
+  // If not, use the default provider for read-only purposes
+  provider = ethers.getDefaultProvider();
+} else {
+  document.getElementById("wallet-status").innerText =
+    "Installed but not connected";
+  // If so, try to connect the wallet
+  provider = new ethers.BrowserProvider(window.ethereum);
+  signer = await provider.getSigner();
+  // Check if the wallet is connected
+  if (signer != null) {
+    document.getElementById("wallet-status").innerText = "Connected";
+  } else {
+    alert("You must connect your wallet to this app before proceeding!");
+  }
+}
+
+// Display the current network
+const currentNetwork = await provider.getNetwork();
+document.getElementById("current-network").innerText =
+  currentNetwork.name.charAt(0).toUpperCase() +
+  currentNetwork.name.slice(1) +
+  " (" +
+  currentNetwork.chainId +
+  ")";
+// Display the current block number
+const blockNumber = await provider.getBlockNumber();
+document.getElementById("block-number").innerText = blockNumber;
+// Display the current account address
+const accountAddress = await signer.getAddress();
+document.getElementById("account-address").innerText = accountAddress;
+// Display the current account balance
+const accountBalance = await provider.getBalance(accountAddress);
+document.getElementById("account-balance").innerText =
+  ethers.formatEther(accountBalance);
+
+// Define the ABI for some functions
+const abi = [
+  // Contract-related APIs
+  "function name() view returns (string)",
+  "function symbol() view returns (string)",
+  "function owner() view returns (address)",
+  "function contractURI() view returns (string)",
+  // Slot-related APIs
+  "function slotDefine(uint256 _slot, string _name)",
+  "function slotInfo(uint256 _slot) view returns (string)",
+  "function slotAllocate(uint256 _slot, address _account)",
+  "function isSlotAllocatedTo(uint256 _slot, address _account) view returns (bool)",
+];
+// Define the contract address
+const contractAddress = "0xc5d59500a8fef16017F2F01D4286dB17C3C18D07";
+// Define the contract
+const contract = new ethers.Contract(contractAddress, abi, signer);
+
+// Display the contract address
+document.getElementById("contract-address").innerText = contractAddress;
+// Display the contract symbol
+const contractName = await contract.name();
+document.getElementById("contract-name").innerText = contractName;
+// Display the contract symbol
+const contractSymbol = await contract.symbol();
+document.getElementById("contract-symbol").innerText = contractSymbol;
+// Display the contract owner
+const contractOwner = await contract.owner();
+document.getElementById("contract-owner").innerText = contractOwner;
+
+// Get and display the contract URI
+document
+  .getElementById("get-contract-uri")
+  .addEventListener("click", async function () {
+    // Retrieve the content of the contract URI
+    const contractURI = await contract.contractURI();
+
+    // Display the SVG image
+    // Convert the string to a Blob object
+    const svgBlob = new Blob([contractURI], {
+      type: "image/svg+xml;charset=utf-8",
+    });
+    // Create a URL for the Blob object
+    const svgUrl = URL.createObjectURL(svgBlob);
+    // Create an <img> element and set its src attribute to the URL
+    const img = document.createElement("img");
+    img.src = svgUrl;
+    img.style.width = "100%";
+    img.style.border = "1px solid black";
+
+    // Append the <img> element to a modal to display it
+    const container = document.getElementById("contract-uri");
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+    container.appendChild(img);
+
+    // Activate the modal
+    const modal = new bootstrap.Modal("#contract-uri-modal", {});
+    modal.show();
+  });
+
+// Define a slot
+document
+  .getElementById("slot-definition-submit")
+  .addEventListener("click", async function () {
+    const id = document.getElementById("slot-definition-id").value;
+    const name = document.getElementById("slot-definition-name").value;
+
+    const tx = await contract.slotDefine(id, name);
+    const receipt = await tx.wait();
+
+    if (receipt.status == 1) {
+      toggleToast(true, `Slot ${id} has been defined as ${name}.`);
+    } else {
+      toggleToast(false, `Slot ${id} failed to be defined as ${name}.`);
+    }
+  });
+// Get and display the slot info
+document
+  .getElementById("slot-info-submit")
+  .addEventListener("click", async function () {
+    const id = document.getElementById("slot-info-id").value;
+    const name = await contract.slotInfo(id);
+    document.getElementById("slot-info-result").innerText = name;
+  });
+// Allocate a slot to an address
+document
+  .getElementById("slot-allocation-submit")
+  .addEventListener("click", async function () {
+    const id = document.getElementById("slot-allocation-id").value;
+    const address = document.getElementById("slot-allocation-address").value;
+
+    const tx = await contract.slotAllocate(id, address);
+    const receipt = await tx.wait();
+
+    if (receipt.status == 1) {
+      toggleToast(true, `Slot ${id} has been allocated to ${address}.`);
+    } else {
+      toggleToast(false, `Slot ${id} failed to be allocated to ${address}.`);
+    }
+  });
+// Check the allocation status of a slot
+document
+  .getElementById("slot-allocation-status-submit")
+  .addEventListener("click", async function () {
+    const id = document.getElementById("slot-allocation-status-id").value;
+    const address = document.getElementById(
+      "slot-allocation-status-address"
+    ).value;
+
+    const status = await contract.isSlotAllocatedTo(id, address);
+
+    document.getElementById("slot-allocation-status-result").innerText = status;
+  });
