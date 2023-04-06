535036af1eda7a35851a4ff0a593540759fe1077
Shaobo ZHANG <hyperzsb@qq.com>
2023-04-06 16:41:37 +0800

    feat(blockchain/erc3525): remove the default deploy script generated by hardhat
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

--- blockchain/erc3525/scripts/deploy.js
+++ blockchain/erc3525/scripts/deploy.js
@@ -1,31 +1,3 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
-
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// This contract is currently deployed by the remix online IDE on the Goerli testnet
+// Current contract address: 0xc5d59500a8fef16017F2F01D4286dB17C3C18D07
+// In the future I may use some script to automate this process
