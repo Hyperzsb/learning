e342d0e0420c45e9453524f651518e6e999b8fe6
Shaobo ZHANG <hyperzsb@qq.com>
2023-05-02 09:37:39 +0800

    feat(go/concurrency): add another example for the dining philosophers problem
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

--- go/concurrency/dining-philosophers/main.go
+++ go/concurrency/dining-philosophers/main.go
@@ -10,12 +10,7 @@ const (
 	diningNum      = 3
 )
 
-func main() {
-	philosophers := make([]philosopher.Philosopher, philosopherNum)
-	for i := range philosophers {
-		philosophers[i] = philosopher.NewPhilosopher(i)
-	}
-
+func withMutex(philosophers []philosopher.Philosopher) {
 	wg := sync.WaitGroup{}
 	diningMutex := sync.Mutex{}
 	forkMutexes := make([]sync.Mutex, philosopherNum)
@@ -48,4 +43,53 @@ func main() {
 	}
 
 	wg.Wait()
+}
+
+func withTryLock(philosophers []philosopher.Philosopher) {
+	wg := sync.WaitGroup{}
+	forkMutexes := make([]sync.Mutex, philosopherNum)
+
+	for i := range philosophers {
+		wg.Add(1)
+		id := i
+		go func() {
+			defer wg.Done()
+			for i := 0; i < diningNum; {
+				// Start to starve
+				philosophers[id].Starving(i)
+				// Try to pick up the fork on the left
+				forkMutexes[id].Lock()
+				// Try to pick up the fork on the right
+				if forkMutexes[(id+1)%philosopherNum].TryLock() {
+					// If getting the fork successfully
+					// Start to eat
+					philosophers[id].Eating(i)
+					// Put down forks on both sides
+					forkMutexes[id].Unlock()
+					forkMutexes[(id+1)%philosopherNum].Unlock()
+					// Start to think
+					philosophers[id].Thinking(i)
+					i++
+				} else {
+					// If not
+					// Put down the fork on the left
+					forkMutexes[id].Unlock()
+					// Start to think
+					philosophers[id].Thinking(i)
+				}
+			}
+		}()
+	}
+
+	wg.Wait()
+}
+
+func main() {
+	philosophers := make([]philosopher.Philosopher, philosopherNum)
+	for i := range philosophers {
+		philosophers[i] = philosopher.NewPhilosopher(i)
+	}
+
+	//withMutex(philosophers)
+	withTryLock(philosophers)
 }
