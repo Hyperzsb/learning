6d68747ddd000db2b0cc3d6ddbf541b1a2a70b21
Shaobo ZHANG <hyperzsb@qq.com>
2023-05-19 12:13:26 +0800

    feat(go/online-shop): add a basic api server
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

---
+++ go/online-shop/cmd/api/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const (
+	version = "0.0.1"
+)
+
+type configuration struct {
+	host string
+	port int
+	env  string
+	db   struct {
+		dsn string
+	}
+	stripe struct {
+		key    string
+		secret string
+	}
+}
+
+type application struct {
+	version string
+	config  configuration
+	loggers struct {
+		info  *log.Logger
+		debug *log.Logger
+		error *log.Logger
+	}
+}
+
+func (app *application) serve() error {
+	server := &http.Server{
+		Addr:              fmt.Sprintf("%s:%d", app.config.host, app.config.port),
+		Handler:           app.router(),
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       30 * time.Second,
+	}
+
+	app.loggers.info.Printf("Starting the API server in %s mode on %s:%d\n", app.config.env, app.config.host, app.config.port)
+
+	return server.ListenAndServe()
+}
+
+func main() {
+	config := configuration{}
+
+	flag.StringVar(&config.host, "host", "127.0.0.1", "host to listen on")
+	flag.IntVar(&config.port, "port", 8000, "port to listen on")
+	flag.StringVar(&config.env, "environment", "dev", "serving mode")
+	flag.StringVar(&config.db.dsn, "dsn", "localhost:3306", "data source name")
+	flag.Parse()
+
+	config.stripe.key = os.Getenv("STRIPE_KEY")
+	config.stripe.secret = os.Getenv("STRIPE_SECRET")
+
+	app := &application{
+		version: version,
+		config:  config,
+		loggers: struct {
+			info  *log.Logger
+			debug *log.Logger
+			error *log.Logger
+		}{
+			info:  log.New(os.Stdout, "[INFO]  ", log.Ldate|log.Ltime),
+			debug: log.New(os.Stdout, "[DEBUG] ", log.Ldate|log.Ltime|log.Lshortfile),
+			error: log.New(os.Stderr, "[ERROR] ", log.Ldate|log.Ltime|log.Lshortfile),
+		},
+	}
+
+	if err := app.serve(); err != nil {
+		app.loggers.error.Fatal(err)
+	}
+}
---
+++ go/online-shop/cmd/api/router.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"net/http"
+)
+
+func (app *application) router() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	mux.Get("/api/test", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("Hello world"))
+	})
+
+	return mux
+}
