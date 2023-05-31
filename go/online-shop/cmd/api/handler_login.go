60d23ad15bb8ae27b7654a3894b324b3783fb323
Shaobo ZHANG <hyperzsb@qq.com>
2023-05-31 17:25:13 +0800

    feat(go/online-shop): improve the login behavior
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

--- go/online-shop/cmd/api/handler_login.go
+++ go/online-shop/cmd/api/handler_login.go
@@ -9,16 +9,28 @@ import (
 	"time"
 )
 
+// loginRequest defines the standard request body of the login API,
+// which will be sent when the user performs a login operation.
 type loginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
+// loginResponse defines the standard response body of the login API,
+// which will be returned in response of the login request.
+// The status code states the result of the login operation:
+// when it equals "Bad Request", the request body is invalid;
+// when it equals "Invalid Credential", the email or password is wrong;
+// when it equals "Internal Server Error", some unexpected errors occur.
+// In these failure cases, the Token field will be meaningless but not be empty.
 type loginResponse struct {
 	Status string      `json:"status"`
 	Token  model.Token `json:"token"`
 }
 
+// login is a handler function to deal with the login operation performed by the user.
+// login will return the session token if the credential provided by the user is correct,
+// and save the token into database to authenticate and track the user in the future.
 func (app *application) login(w http.ResponseWriter, r *http.Request) {
 	app.loggers.info.Printf("%s -> %s\n", r.Method, r.URL)
 
@@ -26,7 +38,11 @@ func (app *application) login(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&request)
 	if err != nil {
 		app.loggers.error.Println(err)
-		http.Error(w, "{}", http.StatusBadRequest)
+		err = loginBadRequest(w)
+		if err != nil {
+			app.loggers.error.Println(err)
+		}
+
 		return
 	}
 
@@ -34,12 +50,15 @@ func (app *application) login(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		app.loggers.error.Println(err)
 		if _, ok := err.(*model.EmptyQueryError); ok {
-			err = invalidCredential(w)
+			err = loginInvalidCredential(w)
 			if err != nil {
 				app.loggers.error.Println(err)
 			}
 		} else {
-			http.Error(w, "{}", http.StatusInternalServerError)
+			err = loginInternalServerError(w)
+			if err != nil {
+				app.loggers.error.Println(err)
+			}
 		}
 
 		return
@@ -49,12 +68,15 @@ func (app *application) login(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		app.loggers.error.Println(err)
 		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
-			err = invalidCredential(w)
+			err = loginInvalidCredential(w)
 			if err != nil {
 				app.loggers.error.Println(err)
 			}
 		} else {
-			http.Error(w, "{}", http.StatusInternalServerError)
+			err = loginInternalServerError(w)
+			if err != nil {
+				app.loggers.error.Println(err)
+			}
 		}
 
 		return
@@ -66,14 +88,22 @@ func (app *application) login(w http.ResponseWriter, r *http.Request) {
 	response.Token, err = model.NewToken(1, "Default Scope", time.Hour*24)
 	if err != nil {
 		app.loggers.error.Println(err)
-		http.Error(w, "{}", http.StatusInternalServerError)
+		err = loginInternalServerError(w)
+		if err != nil {
+			app.loggers.error.Println(err)
+		}
+
 		return
 	}
 
 	responseJSON, err := json.Marshal(response)
 	if err != nil {
 		app.loggers.error.Println(err)
-		http.Error(w, "{}", http.StatusInternalServerError)
+		err = loginInternalServerError(w)
+		if err != nil {
+			app.loggers.error.Println(err)
+		}
+
 		return
 	}
 
@@ -82,9 +112,27 @@ func (app *application) login(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write(responseJSON)
 }
 
-// invalidCredential writes the error message to the response when
-// the email is not existed or the password is not matched
-func invalidCredential(w http.ResponseWriter) error {
+// loginBadRequest writes the error message to the response
+// when the request body does not follow the standard structure.
+func loginBadRequest(w http.ResponseWriter) error {
+	response := loginResponse{
+		Status: "Bad Request",
+	}
+	responseJSON, err := json.Marshal(response)
+	if err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusBadRequest)
+	_, _ = w.Write(responseJSON)
+
+	return nil
+}
+
+// loginInvalidCredential writes the error message to the response
+// when the email is not existed or the password is not matched
+func loginInvalidCredential(w http.ResponseWriter) error {
 	response := loginResponse{
 		Status: "Invalid Credential",
 	}
@@ -95,6 +143,24 @@ func invalidCredential(w http.ResponseWriter) error {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(responseJSON)
+
+	return nil
+}
+
+// loginInternalServerError writes the error message to the response
+// when something goes wrong with the internal logics.
+func loginInternalServerError(w http.ResponseWriter) error {
+	response := loginResponse{
+		Status: "Internal Server Error",
+	}
+	responseJSON, err := json.Marshal(response)
+	if err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusInternalServerError)
 	_, _ = w.Write(responseJSON)
 
 	return nil
