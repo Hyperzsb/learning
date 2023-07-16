74ecfab7f08b8effb03d81049dc3e017dc552c4a
Shaobo ZHANG <hyperzsb@qq.com>
2023-07-16 19:21:55 +0800

    feat(java/logging): add slf4j and log4j2 demos
    Signed-off-by: Shaobo ZHANG <hyperzsb@qq.com>

--- java/logging/src/main/java/learning/controller/CarController.java
+++ java/logging/src/main/java/learning/controller/CarController.java
@@ -2,18 +2,24 @@ package learning.controller;
 
 import learning.bean.Car;
 import learning.service.CarService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-
+@Slf4j
 @RestController
 public class CarController {
-    @Resource
     private CarService carService;
+
+    @Autowired
+    public void setCarService(CarService carService) {
+        this.carService = carService;
+    }
 
     @GetMapping("/car")
     public Car getCar() {
+        log.info("GET /car");
         return carService.getCar();
     }
 }
